3960ac1d9f2a154e22c9dcd976e6ee0fc26769ad
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-08-21 10:50:28 +0100

    Update arm64v8 docker build

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -18,12 +18,12 @@ cd $PROJECT_ROOT
 # build all the images
 docker build -t ${basetag}-amd64 -f build/package/go-load-balancer/Dockerfile.amd64 .
 docker build -t ${basetag}-arm32v7 -f build/package/go-load-balancer/Dockerfile.arm32v7 .
-docker build -t ${basetag}-aarch64 -f build/package/go-load-balancer/Dockerfile.aarch64 .
+docker build -t ${basetag}-arm64v8 -f build/package/go-load-balancer/Dockerfile.arm64v8 .
 
 # # push em all
 docker push ${basetag}-amd64 &
 docker push ${basetag}-arm32v7 &
-docker push ${basetag}-aarch64 &
+docker push ${basetag}-arm64v8 &
 
 wait
 
@@ -33,9 +33,10 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker manifest create ${basetag} \
 	${basetag}-amd64 \
 	${basetag}-arm32v7 \
-	${basetag}-aarch64
+	${basetag}-arm64v8
 
 # explicit annotations
+docker manifest annotate ${basetag} ${basetag}-arm64v8 --os "linux" --arch "arm64" --variant "v8"
 docker manifest annotate ${basetag} ${basetag}-arm32v7 --os "linux" --arch "arm" --variant "v7"
 
 # ship it
